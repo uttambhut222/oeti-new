a0c010fdd45e00a5cef8c52fd66b7cfad84a8965
Uttam Bhut <uttam.bhut@theopeneyes.in>
2018-10-05 07:47:48 -0400

    ubc

--- resume.php
+++ resume.php
@@ -3,8 +3,11 @@
   <title>Learn Feedback Demo - User Record</title>
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1">
-  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  
+  <link type="text/css" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.1/css/bootstrap.css" />
+  <link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" />
   <link type="text/css" rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/jquery.dataTables.min.css" />
+  
   <style>
 	div.dataTables_wrapper {
         width: 99%;
@@ -17,32 +20,14 @@
 <h1 style="padding:8px 0">Resume</h1>
 
 <?php
-/*
-$servername = "localhost";
-$username = "devbyo5_LFBDEMO";
-$password = "o7N[;j]U*6y5";
-$dbname = "devbyo5_LFBDEMO";
-*/
-///*
-$servername = "localhost";
-$username = "root";
-$password = "";
-$dbname = "oeti";
-//*/
-
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+include("config.php");
 
 $sql = "SELECT `CareerId`, `Position`, `FirstName`, `LastName`, `CurrentLocation`, `Email`, `Phone`, `Qualification`, `Experience`, `Salary`, `EmploymentType`, `Resume`, `CreatedOn` FROM `tblcareer` ORDER by `CareerId` DESC;";
 $result = mysqli_query($conn, $sql);
 
 if (mysqli_num_rows($result) > 0) {
     // output data of each row
-	echo '<table id="example" class="display nowrap" style="width:100%">';
+	echo '<table id="example" class="display table table-striped table-bordered" style="width:100%">';
 	$sr = 1;
 	echo "
 		<thead>
@@ -93,15 +78,16 @@ mysqli_close($conn);
 
 </div>
 
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.3.1.js"></script>
 <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
 	
 <script>
 $(document).ready(function() {
     $('#example').DataTable( {
-        "scrollX": true
+		"scrollX": true,
+		"scrollY": '60vh',
+        "scrollCollapse": true,
     } );
 } );
 </script>
